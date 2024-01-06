5e984dce169ea49d55176d90e32bab6b41628965
Adrian Jendo <ajendo@uwaterloo.ca>
2024-01-05 23:32:13 -0800

    Setup swift project

---
+++ lynx-mobile/lynx-mobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lynx-mobile
+//
+//  Created by Adrian Jendo on 2024-01-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
